4658ba1b30448d7b66b9644cd3f05c81fbd95e4d
caleb1248 <94559723+caleb1248@users.noreply.github.com>
2022-01-29 09:08:59 -0500

    made the audio recorder, the file part will come soon

--- voice2file.js
+++ voice2file.js
@@ -1,1 +1,27 @@
+const button = document.querySelector('button'),
+    audio = document.querySelector('audio');
+function recordMedia() {
+    navigator.mediaDevices.getUserMedia({
+        video: false,
+        audio: true
+    }).then(stream => {
+        var recorder = new MediaRecorder(stream),
+            chunks = [];
 
+        recorder.start();
+        
+        recorder.ondataavailable = function(e){
+            chunks.push(e.data);
+        };
+        recorder.onstop = function(){
+            const blob = new Blob(chunks);
+            audio.src = URL.createObjectURL(blob);
+        }
+        button.innerHTML = "Stop";
+        button.onclick = function () {
+            recorder.stop();
+            button.innerHTML = "Start"
+            button.onclick = recordMedia;
+        }
+    })
+}
